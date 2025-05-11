b557ac76c9330dd1675b25361a78bc9e21a30243
M-Elsaied <31868939+M-Elsaied@users.noreply.github.com>
2025-05-11 17:40:25 +0300

    Update start.sh nsflow local agent

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ export AGENT_MANIFEST_FILE=registries/manifest.hocon
 export AGENT_TOOL_PATH=coded_tools
 export AGENT_MANIFEST_UPDATE_PERIOD_SECONDS=5
 
+# IMPORTANT: tell nsflow to use local agent loading
+export NS_SERVER_HOST=local
+export NS_SERVER_PORT=0
+
 # Start Neuro-SAN backend in the background
 python -u -m neuro_san.service.agent_main_loop --port $NEURO_SAN_SERVER_PORT &
 
